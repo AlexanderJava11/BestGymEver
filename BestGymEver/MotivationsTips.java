b453c167a6cfd81604da1d0810c5fd086805b9db
Aleks <alexander.zeljic@yh.nackademin.se>
2025-10-22 23:22:14 +0200

    Motivationstips

---
+++ BestGymEver/MotivationsTips.java
@@ -0,0 +1,16 @@
+import java.util.List;
+import java.util.Random;
+
+public class MotivationsTips {
+    private static final List<String> tips = List.of(
+            "Kom ihåg: du ångrar aldrig ett träningspass!",
+            "Träna för att må bra, inte bara för att se bra ut \uD83D\uDCAA",
+            "Lite träning är bättre än ingen träning alls!",
+            "Svetten är bara fett som gråter \uD83D\uDE05",
+            "Ge inte upp, varje steg räknas");
+
+    public static String slumpaTips() {
+        Random rand = new Random();
+        return tips.get(rand.nextInt(tips.size()));
+    }
+}
